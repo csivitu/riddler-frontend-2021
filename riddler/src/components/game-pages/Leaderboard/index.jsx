51304d2288ebc7ee8ef63d513f503d8f83ace9f3
Anuka04 <anoushkachakravorty.5@gmail.com>
2021-08-12 23:02:10 +0530

    leaderboard page

--- riddler/src/components/game-pages/Leaderboard/index.jsx
+++ riddler/src/components/game-pages/Leaderboard/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getLeaderboard } from "../../../api/api";
+import './leader.css';
 
 const Leaderboard = () => {
   const [leaderboard, setLeaderboard] = useState([]);
@@ -12,22 +13,44 @@ const Leaderboard = () => {
   }, [leaderboard]);
 
   return(
-    <table>
+    <div className="whole_page">
+    <table className="tables scrolldown">
         <tr>
-            <th>Rank</th>
-            <th>Player</th>
-            <th>Score</th>
+            <th className="heading">Rank</th>
+            <th className="heading">Player</th>
+            <th className="heading">Score</th>
+        </tr>
+        <tr className="names">
+        <td>1</td>
+                  <td>Anoushka</td>
+                  <td>20000</td>
+        </tr>
+        <tr className="names">
+                  <td>2</td>
+                  <td>Sanjay</td>
+                  <td>20000</td>
+        </tr>
+        <tr className="names">
+                  <td>3</td>
+                  <td>Subhanu</td>
+                  <td>20000</td>
+        </tr>
+        <tr className="names">
+                  <td>4</td>
+                  <td>Nimish</td>
+                  <td>20000</td>
         </tr>
       {leaderboard.map((item,rank)=>{
           return (
-              <tr>
-                  <th>{rank+1}</th>
-                  <th>{item.username}</th>
-                  <th>{item.score}</th>
+              <tr className="names">
+                  <td>{rank+1}</td>
+                  <td>{item.username}</td>
+                  <td>{item.score}</td>
               </tr>
           )
       })}
       </table>
+      </div>
   )
 };
 
